f663dfb47258a1ea3a613b809ab80ff97db0ffef
Nihat Benli <nihatbenli.nb@gmail.com>
2021-08-30 16:10:49 +0200

    add custom validation hook

--- src/components/User/Add/AddUser.js
+++ src/components/User/Add/AddUser.js
@@ -1,31 +1,76 @@
 import React from "react";
+import useInput from "../../hooks/use-input";
 import Button from "../../UI/Button/Button";
 import Card from "../../UI/Card/Card";
 import classes from "./AddUser.module.css";
 
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+
 const AddUser = (props) => {
+  /* Custom hook */
+  const {
+    value: enteredName,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameInputHandler,
+    reset: resetName,
+  } = useInput(isNotEmpty);
+  const {
+    value: enteredSurname,
+    isValid: surnameIsValid,
+    hasError: surnameHasError,
+    valueChangeHandler: surnameChangeHandler,
+    inputBlurHandler: surnameBlurHandler,
+    reset: resetSurname,
+  } = useInput(isNotEmpty);
+  const {
+    value: enteredEmail,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+  const {
+    value: enteredPassword,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPassword,
+  } = useInput(isNotEmpty);
+  const {
+    value: enteredPhone,
+    isValid: phoneIsValid,
+    hasError: phoneHasError,
+    valueChangeHandler: phoneChangeHandler,
+    inputBlurHandler: phoneBlurHandler,
+    reset: resetPhone,
+  } = useInput(isNotEmpty);
   return (
     <Card className={classes.input}>
       <form>
         <div>
           <label htmlFor="name">Name</label>
-          <input id="name" type="text" />
+          <input id="name" type="text" value={enteredName} />
         </div>
         <div>
           <label htmlFor="surname">Surname</label>
-          <input id="surname" type="text" />
+          <input id="surname" type="text" value={enteredSurname} />
         </div>
         <div>
           <label htmlFor="email">E-mail</label>
-          <input id="email" type="text" />
+          <input id="email" type="text" value={enteredEmail} />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" />
+          <input id="password" type="password" value={enteredPassword} />
         </div>
         <div>
           <label htmlFor="phone">Telephone</label>
-          <input id="phone" type="text" />
+          <input id="phone" type="text" value={enteredPhone} />
         </div>
         <Button>Submit</Button>
       </form>
