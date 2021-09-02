70e3f327aebbc43307c02c3b5f27cd317bd36ee6
Nihat Benli <nihatbenli.nb@gmail.com>
2021-09-02 17:49:03 +0200

    add login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,25 @@
 import React from "react";
 import Card from "../UI/Card/Card";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  return <Card></Card>;
+  return (
+    <Card>
+      <form>
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input type="text" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div>
+          <Button>Login</Button>
+        </div>
+      </form>
+    </Card>
+  );
 };
 
 export default Login;
