023e792266ef07b61b0d947519a57a921761a7dd
Nihat Benli <nihatbenli.nb@gmail.com>
2021-09-02 20:07:06 +0200

    add , check is user logged in with useEffect and local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import MainHeader from "./components/Header/MainHeader/MainHeader";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
@@ -6,11 +6,21 @@ import Login from "./components/Login/Login";
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
